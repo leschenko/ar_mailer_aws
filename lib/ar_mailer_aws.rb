c4b6ea3f30cd30a70ef81f7cdc8e548e3115db0c
Alex Leschenko <leschenko.al@gmail.com>
2013-07-03 14:21:38 +0300

    cmd options more user friendly

--- lib/ar_mailer_aws.rb
+++ lib/ar_mailer_aws.rb
@@ -35,6 +35,8 @@ module ArMailerAWS
     end
 
     def parse_options(args)
+      start_i = args.index('--').try(:succ) || 0
+      args = args[start_i..-1]
       options = OpenStruct.new
 
       OptionParser.new do |opts|
@@ -44,10 +46,30 @@ module ArMailerAWS
         options.rate = 5
         options.max_age = 3600 * 24 * 7
 
-        opts.banner = 'Usage: ar_mailer_aws [options] COMMAND'
+        opts.banner = <<-TXT.strip_heredoc
+          Usage: ar_mailer_aws <command> <options> -- <application options>
+
+          * where <command> is one of:
+            start         start an instance of the application
+            stop          stop all instances of the application
+            restart       stop all instances and restart them afterwards
+            reload        send a SIGHUP to all instances of the application
+            run           start the application and stay on top
+            zap           set the application to a stopped state
+            status        show status (PID) of application instances
+
+          * where <options> may contain several of the following:
+
+              -t, --ontop                      Stay on top (does not daemonize)
+              -f, --force                      Force operation
+              -n, --no_wait                    Do not wait for processes to stop
+
+          * and where <application options> may contain several of the following:
+
+        TXT
 
         opts.on('-b', '--batch-size BATCH_SIZE', 'Maximum number of emails to send per delay',
-                'Default: Deliver all available emails', Integer) do |batch_size|
+                "Default: #{options.batch_size}", Integer) do |batch_size|
           options.batch_size = batch_size
         end
 
@@ -56,7 +78,7 @@ module ArMailerAWS
           options.delay = delay
         end
 
-        opts.on('-q', '--quota QUOTA', 'Quota of emails per day', "Default: #{options.quota}", Integer) do |quota|
+        opts.on('-q', '--quota QUOTA', 'Daily quota for sending emails', "Default: #{options.quota}", Integer) do |quota|
           options.quota = quota
         end
 
@@ -77,7 +99,7 @@ module ArMailerAWS
           options.verbose = v
         end
 
-        opts.on('-h', '--help', 'Show this message') do
+        opts.on_tail('-h', '--help', 'Show this message') do
           puts opts
           exit
         end
