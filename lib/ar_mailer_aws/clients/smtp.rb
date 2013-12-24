0083568bc3da8679ba5d58fd7e02b567c49e973f
Alex Leschenko <leschenko.al@gmail.com>
2013-12-25 00:01:15 +0200

    smtp mailer cleanup

--- lib/ar_mailer_aws/clients/smtp.rb
+++ lib/ar_mailer_aws/clients/smtp.rb
@@ -5,13 +5,13 @@ module ArMailerAWS
     class SMTP < Base
 
       def send_emails(emails)
-        @service = Net::SMTP.new(settings[:address], settings[:port])
-        @service.enable_starttls_auto if settings[:enable_starttls_auto]
-        @service.start(settings[:domain], settings[:user_name], settings[:password], settings[:authentication]) do |smtp|
+        session = Net::SMTP.new(settings[:address], settings[:port])
+        session.enable_starttls_auto if settings[:enable_starttls_auto]
+        session.start(settings[:domain], settings[:user_name], settings[:password], settings[:authentication]) do |smtp|
           emails.each do |email|
             begin
               break if exceed_quota?
-              smtp.send_message email.mail, email.from, email.to
+              send_email(smtp, email)
             rescue => e
               handle_error(e, email)
             end
@@ -19,9 +19,9 @@ module ArMailerAWS
         end
       end
 
-      def send_email(email)
+      def send_email(smtp, email)
         log "send email to #{email.to}"
-        @service.send_message email.mail, email.from, email.to
+        log smtp.send_message(email.mail, email.from, email.to)
         email.destroy
         @sent_count += 1
       end
